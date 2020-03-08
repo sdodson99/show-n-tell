c1a55df1126a6aacc44c43cb5e37c2ebaed71bb0
Sean Dodson <dodsonsean@comcast.net>
2020-03-08 16:58:19 -0400

    Add navigation bar styling

--- src/show-n-tell-vue/src/components/App.vue
+++ src/show-n-tell-vue/src/components/App.vue
@@ -15,4 +15,29 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a:hover, a:active, a:link{
+    color: inherit;
+    text-decoration: none;
+  }
+  
+  #app{
+    --color-primary-dark: rgb(49, 60, 71);
+    --color-primary-medium: rgb(87, 107, 126);
+    --color-primary-light: rgb(134, 164, 199);
+    
+    --color-secondary-dark: rgb(241, 158, 13);
+    --color-secondary-medium: rgb(236, 182, 89);
+    --color-secondary-light: rgb(245, 230, 204);
+
+    --color-grayscale-dark: rgb(53, 53, 53);
+    --color-grayscale-medium: rgb(121, 121, 121);
+    --color-grayscale-light: rgb(230, 230, 230);
+  }
+</style>
