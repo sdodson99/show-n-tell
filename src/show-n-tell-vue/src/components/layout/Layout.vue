8b2b2a172842fc41270020a230dc0b98832f973a
Sean Dodson <dodsonsean@comcast.net>
2020-03-15 13:48:58 -0400

    Add messaging banner

--- src/show-n-tell-vue/src/components/layout/Layout.vue
+++ src/show-n-tell-vue/src/components/layout/Layout.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <NavigationBar v-bind:is-logged-in="isLoggedIn" />
+    <div v-if="statusMessage" :class="alertClass" class="alert text-center">
+      {{ statusMessage }}
+    </div>
     <div class="container">
-      <router-view class="m-5"></router-view>
+      <router-view class="m-5"
+        @alert-success="alertSuccess"
+        @alert-status="alertStatus"
+        @alert-error="alertError"></router-view>
     </div>
   </div>
 </template>
@@ -17,6 +23,33 @@ export default {
   },
   props: {
     isLoggedIn: Boolean
+  },
+  data: function() {
+    return {
+      statusMessage: "",
+      alertClass: "" || "alert-primary",
+      timeoutHandle: 0
+    }
+  },
+  methods: {
+    alertSuccess: function(message, duration) {
+      this.alertClass = "alert-success"
+      this._setMessage(message, duration)
+    },
+    alertStatus: function(message, duration) {
+      this.alertClass = "alert-primary"
+      this._setMessage(message, duration)
+    },
+    alertError: function(message, duration) {
+      this.alertClass = "alert-danger"
+      this._setMessage(message, duration)
+
+    },
+    _setMessage: function(message, duration) {
+      this.statusMessage = message
+      clearTimeout(this.timeoutHandle)
+      this.timeoutHandle = setTimeout(() => this.statusMessage = "", duration || 3000)
+    }
   }
 };
 </script>
