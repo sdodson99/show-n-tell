62c1744540912b8491afeb0b995079a0145b7edb
Sean Dodson <dodsonsean@comcast.net>
2020-03-15 19:51:44 -0400

    Add like and comment migrations

---
+++ src/api/ShowNTell.EntityFramework/Migrations/20200315235103_like_comment.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShowNTell.EntityFramework.Migrations
+{
+    public partial class like_comment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserEmail = table.Column<string>(nullable: true),
+                    ImagePostId = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_ImagePosts_ImagePostId",
+                        column: x => x.ImagePostId,
+                        principalTable: "ImagePosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Comments_Users_UserEmail",
+                        column: x => x.UserEmail,
+                        principalTable: "Users",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Likes",
+                columns: table => new
+                {
+                    UserEmail = table.Column<string>(nullable: false),
+                    ImagePostId = table.Column<int>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Likes", x => new { x.ImagePostId, x.UserEmail });
+                    table.ForeignKey(
+                        name: "FK_Likes_ImagePosts_ImagePostId",
+                        column: x => x.ImagePostId,
+                        principalTable: "ImagePosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Likes_Users_UserEmail",
+                        column: x => x.UserEmail,
+                        principalTable: "Users",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ImagePostId",
+                table: "Comments",
+                column: "ImagePostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_UserEmail",
+                table: "Comments",
+                column: "UserEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_UserEmail",
+                table: "Likes",
+                column: "UserEmail");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Likes");
+        }
+    }
+}
