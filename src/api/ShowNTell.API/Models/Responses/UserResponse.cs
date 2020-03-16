fec2bebafd277de856e04a1d89f7d3095360c642
sdodson99 <dodsonsean@comcast.net>
2020-03-15 21:06:02 -0400

    Add like icon updates

--- src/api/ShowNTell.API/Models/Responses/UserResponse.cs
+++ src/api/ShowNTell.API/Models/Responses/UserResponse.cs
@@ -4,8 +4,8 @@ namespace ShowNTell.API.Models.Responses
 {
     public class UserResponse
     {
-        public int Id { get; set; }
         public string Email { get; set; }
+        public string Username { get; set; }
         public DateTime DateJoined { get; set; }
     }
 }
