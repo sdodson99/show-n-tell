41dabefb78c37a132432686b4e7cef4fa6a3bb86
Sean Dodson <dodsonsean@comcast.net>
2020-03-20 17:07:55 -0400

    Add comment service

---
+++ src/api/ShowNTell.EntityFramework.Tests/Services/EFCommentServiceTest.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using ShowNTell.Domain.Models;
+using ShowNTell.EntityFramework.Services;
+using ShowNTell.EntityFramework.ShowNTellDbContextFactories;
+
+namespace ShowNTell.EntityFramework.Tests.Services
+{
+    [TestFixture]
+    public class EFCommentServiceTest
+    {
+        private EFCommentService _commentService;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            Mock<IShowNTellDbContextFactory> contextFactory = new Mock<IShowNTellDbContextFactory>();
+            contextFactory.Setup(c => c.CreateDbContext()).Returns(() => GetDbContext());
+
+            _commentService = new EFCommentService(contextFactory.Object);
+        }   
+
+        [Test]
+        public async Task Create_WithValidComment_ReturnsCreatedCommentWithNewIdAndExpectedContent()
+        {
+            Comment newComment = new Comment()
+            {
+                Content = "hello world"
+            };
+            string expectedContent = newComment.Content;
+            int nonExpectedId = newComment.Id;
+
+            Comment createdComment = await _commentService.Create(newComment);
+            string actualContent = createdComment.Content;
+            int actualId = createdComment.Id;
+
+            Assert.AreEqual(expectedContent, actualContent);
+            Assert.AreNotEqual(nonExpectedId, actualId);
+        }
+
+        private ShowNTellDbContext GetDbContext()
+        {
+            DbContextOptions options = new DbContextOptionsBuilder().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            ShowNTellDbContext context = new ShowNTellDbContext(options);
+
+            return context;
+        }
+    }
+}
