d36bf805a5c25fcd220e8fa34dd582c85c7c19d1
Sean Dodson <dodsonsean@comcast.net>
2020-03-21 20:26:59 -0400

    Remove EXIF reading for non-JPEGs

--- src/show-n-tell-vue/src/components/image-posts/ImagePostImage.vue
+++ src/show-n-tell-vue/src/components/image-posts/ImagePostImage.vue
@@ -1,5 +1,7 @@
 <template>
-    <div id="image-container" @click="$emit('click')" class="d-flex align-items-center justify-content-center">
+    <div id="root">
+        <div id="image-container" ref="imageContainer" @click="$emit('click')" class="d-flex align-items-center justify-content-center">
+        </div>
     </div>
 </template>
 
@@ -26,32 +28,55 @@ export default {
     },
     methods: {
         loadImage: function() {
-            this.$el.innerHTML = ""
+            this.clearImage()
 
             LoadImage(this.imageUri, (loadedImage) => {
-                EXIF.getData(loadedImage, () => {
-                    let orientation = EXIF.getTag(loadedImage, "Orientation");
-                    
-                    LoadImage(loadedImage.src, (orientedImage) => {
-                        orientedImage.style.maxHeight = this.maxHeight
-                        orientedImage.style.maxWidth = "100%"
-
-                        this.$el.appendChild(orientedImage)
-                    }, {
-                        orientation: orientation
+                
+                if(this.isJPEG(loadedImage.src)) {
+                    EXIF.getData(loadedImage, () => {
+                        let orientation = EXIF.getTag(loadedImage, "Orientation");
+                        
+                        LoadImage(loadedImage.src, (orientedImage) => {
+                            this.setImage(orientedImage)
+                        }, {
+                            orientation: orientation
+                        })
                     })
-                })
+                } else {
+                    this.setImage(loadedImage)
+                }
+
             })  
+        },
+        setImage: function(image) {
+            image.removeAttribute('height')
+            image.removeAttribute('width')
+            image.style.maxHeight = this.maxHeight
+            image.style.maxWidth = "100%"
+
+            this.$refs.imageContainer.appendChild(image)
+        },
+        clearImage: function() {
+            this.$refs.imageContainer.innerHTML = ""
+        },        
+        isJPEG: function(src) {
+            return src.endsWith('jpeg') || src.endsWith('jpg')
         }
     }
 }
 </script>
 
 <style scoped>
+#root {
+    height: 100%;
+    min-height: 100px;
+}
+
 #image-container{
     background: var(--color-grayscale-light);
     border-radius: 3px;
     border: 1px solid var(--color-primary-dark);
+    min-height: 100px;
     height: 100%;
 }
 </style>
