bf862fbcab1ace2be995b35d64361932e1522b70
Sean Dodson <dodsonsean@comcast.net>
2020-03-25 20:49:03 -0400

    Fix navigation bar dropdown delay

--- src/show-n-tell-vue/src/components/layout/NavigationBar.vue
+++ src/show-n-tell-vue/src/components/layout/NavigationBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav-root">
     <div class="container">
-      <nav :class="{ open: open }" class="row p-3 align-items-center justify-content-center">
+      <nav class="row p-3 align-items-center justify-content-center">
         <div class="col-lg-auto d-flex flex-column align-items-center justify-content-center">
           <router-link
             class="row justify-content-center mx-1 link"
@@ -10,60 +10,60 @@
             <img id="logo" class="ml-3 ml-lg-0" src="../../assets/showntell.png"/>
           </router-link>
           <Hamburger id="ham" class="d-lg-none mt-3" 
+            v-b-toggle.menu-items
             @toggleOn="opened" @toggleOff="closed" :toggled="open"
             toggle-on-style="stroke: var(--color-secondary-medium)"
             toggle-off-style="stroke: var(--color-primary-dark)"></Hamburger>
         </div>
         <div class="d-none d-lg-block col"/>
-        <div id="menu-items" :class="{ open: open }" class="col-lg-auto row align-items-center">
+        <b-collapse id="menu-items" class="col-lg-auto row align-items-center">
           <router-link
             class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
             active-class="active-link"
-            to="/explore"
-          >
+            to="/explore">
             <div class="content px-3 p-2">Explore</div>
           </router-link>
+
           <router-link
             v-if="isLoggedIn"
             class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
             active-class="active-link"
-            to="/feed"
-          >
+            to="/feed">
             <div class="content px-3 p-2">Feed</div>
           </router-link>
+
           <router-link
             v-if="isLoggedIn"
             class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
             active-class="active-link"
-            to="/create"
-          >
+            to="/create">
             <div class="content px-3 p-2">Create</div>
           </router-link>
+
           <router-link
             v-if="isLoggedIn"
             class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
             active-class="active-link"
-            to="/profile"
-          >
+            to="/profile">
             <div class="content px-3 p-2">Profile</div>
           </router-link>
+
           <router-link
             v-if="!isLoggedIn"
             class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
             active-class="active-link"
-            to="/login"
-          >
+            to="/login">
             <div class="content px-3 p-2">Login</div>
           </router-link>
+
           <router-link
             v-if="isLoggedIn"
             class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
             active-class="active-link"
-            to="/logout"
-          >
+            to="/logout">
             <div class="content px-3 p-2">Logout</div>
           </router-link>
-        </div>
+        </b-collapse>
       </nav>
     </div>
   </div>
@@ -133,19 +133,9 @@ export default {
   border-bottom: 3px solid var(--color-secondary-dark);
 }
 
-#menu-items {
-  max-height: 0;
-  transition: max-height 1s;
-  overflow: hidden;
-}
-
-#menu-items.open {
-  max-height: 500px;
-}
-
 @media screen and (min-width: 992px) {
-  #menu-items, #menu-items.open {
-    max-height: 500px;
+  #menu-items{
+    display: flex !important;
   }
 }
 </style>
