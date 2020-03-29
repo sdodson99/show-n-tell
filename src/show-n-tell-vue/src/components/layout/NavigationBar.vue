527bdbb7e2286e78b405e1b4bb3bd98a109b86c2
Sean Dodson <dodsonsean@comcast.net>
2020-03-29 15:36:14 -0400

    Fix navigation bar toggle and add search bar

--- src/show-n-tell-vue/src/components/layout/NavigationBar.vue
+++ src/show-n-tell-vue/src/components/layout/NavigationBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav-root">
     <div class="container">
-      <nav class="row p-3 align-items-center justify-content-center">
+      <nav class="d-flex flex-column flex-lg-row p-3 align-items-center justify-content-center">
         <div class="col-lg-auto d-flex flex-column align-items-center justify-content-center">
           <router-link
             class="row justify-content-center mx-1 link"
@@ -15,53 +15,57 @@
             toggle-on-style="stroke: var(--color-secondary-medium)"
             toggle-off-style="stroke: var(--color-primary-dark)"></Hamburger>
         </div>
-        <div class="d-none d-lg-block col"/>
-        <b-collapse id="menu-items" class="col-lg-auto row align-items-center">
+        <b-collapse id="menu-items" class="flex-grow-1 flex-column flex-lg-row align-items-center justify-content-lg-end">
+          <form id="search-form" class="flex-grow-1 form-inline mx-4 my-2">
+            <input class="form-control w-100" type="search" placeholder="Search" aria-label="Search">
+            <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+          </form>
+
           <router-link
-            class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
+            class="row justify-content-center text-center link hover-link"
             active-class="active-link"
             to="/explore">
-            <div class="content px-3 p-2">Explore</div>
+            <div class="content px-1 p-2">Explore</div>
           </router-link>
 
           <router-link
             v-if="isLoggedIn"
-            class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
+            class="row justify-content-center text-center link hover-link"
             active-class="active-link"
             to="/feed">
-            <div class="content px-3 p-2">Feed</div>
+            <div class="content px-1 p-2">Feed</div>
           </router-link>
 
           <router-link
             v-if="isLoggedIn"
-            class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
+            class="row justify-content-center text-center link hover-link"
             active-class="active-link"
             to="/create">
-            <div class="content px-3 p-2">Create</div>
+            <div class="content px-1 p-2">Create</div>
           </router-link>
 
           <router-link
             v-if="isLoggedIn"
-            class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
+            class="row justify-content-center text-center link hover-link"
             active-class="active-link"
             to="/profile">
-            <div class="content px-3 p-2">Profile</div>
+            <div class="content px-1 p-2">Profile</div>
           </router-link>
 
           <router-link
             v-if="!isLoggedIn"
-            class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
+            class="row justify-content-center text-center link hover-link"
             active-class="active-link"
             to="/login">
-            <div class="content px-3 p-2">Login</div>
+            <div class="content px-1 p-2">Login</div>
           </router-link>
 
           <router-link
             v-if="isLoggedIn"
-            class="col-lg-auto row mx-1 justify-content-center text-center link hover-link"
+            class="row justify-content-center text-center link hover-link"
             active-class="active-link"
             to="/logout">
-            <div class="content px-3 p-2">Logout</div>
+            <div class="content px-1 p-2">Logout</div>
           </router-link>
         </b-collapse>
       </nav>
@@ -107,6 +111,10 @@ export default {
   max-width: 100%;
 }
 
+#menu-items {
+  display: flex;
+}
+
 #nav-root {
   background: var(--color-grayscale-light);
   color: var(--color-primary-dark);
@@ -118,10 +126,15 @@ export default {
   text-decoration: none;
   color: inherit;
   cursor: pointer;
+  margin: 0 10px;
+}
+
+.content {
+  font-size: 0.75em;
 }
 
 .link .content {
-  min-width: 100px;
+  min-width: 75px;
   border-bottom: 3px solid var(--color-grayscale-light);
 }
 
@@ -131,6 +144,10 @@ export default {
 
 .hover-link .content:hover {
   border-bottom: 3px solid var(--color-secondary-dark);
+}
+
+#search-form {
+  max-width: 300px;
 }
 
 @media screen and (min-width: 992px) {
