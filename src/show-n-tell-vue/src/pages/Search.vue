1d00f3974f01a640f97c2fe338773dc10bdf9a14
Sean Dodson <dodsonsean@comcast.net>
2020-03-29 21:52:46 -0400

    Add search page redirect

---
+++ src/show-n-tell-vue/src/pages/Search.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Search",
+    props: {
+        query: String,
+        currentUser: Object
+    },
+    watch: {
+        query: async function() {
+            console.log(this.query);    
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
