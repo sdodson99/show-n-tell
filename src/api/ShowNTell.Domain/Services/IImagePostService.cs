d28c9a6089ce6a651aa13449e74cdfc4f9bceed1
Sean Dodson <dodsonsean@comcast.net>
2020-04-04 14:05:46 -0400

    Add image post service tag update

--- src/api/ShowNTell.Domain/Services/IImagePostService.cs
+++ src/api/ShowNTell.Domain/Services/IImagePostService.cs
@@ -11,6 +11,7 @@ namespace ShowNTell.Domain.Services
         Task<ImagePost> GetById(int id);
         Task<ImagePost> Create(ImagePost imagePost);
         Task<ImagePost> Update(int id, string description);
+        Task<ImagePost> Update(int id, string description, IEnumerable<Tag> tags);
         Task<bool> Delete(int id);
         Task<bool> IsAuthor(int id, string email);
     }
