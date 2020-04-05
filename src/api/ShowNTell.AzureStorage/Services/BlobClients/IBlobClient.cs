76fbc164965380e1bd67f1fcdb97a9a09379f92a
Sean Dodson <dodsonsean@comcast.net>
2020-04-05 10:44:26 -0400

    Add Azure storage documentation

--- src/api/ShowNTell.AzureStorage/Services/BlobClients/IBlobClient.cs
+++ src/api/ShowNTell.AzureStorage/Services/BlobClients/IBlobClient.cs
@@ -6,11 +6,26 @@ using Azure.Storage.Blobs.Models;
 
 namespace ShowNTell.AzureStorage.Services.BlobClients
 {
+    /// <summary>
+    /// A client for uploading and deleting blobs.
+    /// </summary>
     public interface IBlobClient
     {
         Uri Uri { get; }
         
+        /// <summary>
+        /// Upload a new blob.
+        /// </summary>
+        /// <param name="blobName">The name of the new blob.</param>
+        /// <param name="stream">The content of the blob.</param>
+        /// <returns>The blob upload result.</returns>
         Task<Response<BlobContentInfo>> UploadBlobAsync(string blobName, Stream stream);
+
+        /// <summary>
+        /// Delete a blob.
+        /// </summary>
+        /// <param name="blobName">The name of the blob to delete.</param>
+        /// <returns>The blob delete result.</returns>
         Task<Response> DeleteBlobAsync(string blobName);
     }
 }
