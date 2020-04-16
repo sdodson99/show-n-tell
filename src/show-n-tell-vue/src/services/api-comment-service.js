f82f644396a5f056e4a332bdb1f005c7c04497af
Sean Dodson <dodsonsean@comcast.net>
2020-04-15 20:16:35 -0400

    Add update and delete to client comment service

--- src/show-n-tell-vue/src/services/api-comment-service.js
+++ src/show-n-tell-vue/src/services/api-comment-service.js
@@ -34,6 +34,47 @@ class APICommentService{
 
         return Comment.fromJSON(commentResponse)
     }
+
+    /**
+     * Update the content of a comment.
+     * @param {number} imagePostId The id of the image post with the comment.
+     * @param {number} commentId The id of the comment.
+     * @param {string} content The content for the comment to update.
+     */
+    async updateComment(imagePostId, commentId, content) {
+        const url = `${this.baseUrl}/imageposts/${imagePostId}/comments/${commentId}`
+
+        const commentRequest = {
+            content: content
+        }
+
+        const apiResponse = await this.apiClient.authFetch(url, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(commentRequest)
+        });
+
+        const commentResponse = await apiResponse.json();
+
+        return Comment.fromJSON(commentResponse)
+    }
+
+    /**
+     * Delete a comment.
+     * @param {number} imagePostId The id of the image post with the comment.
+     * @param {number} commentId The id of the comment.
+     */
+    async deleteComment(imagePostId, commentId) {
+        const url = `${this.baseUrl}/imageposts/${imagePostId}/comments/${commentId}`
+
+        const apiResponse = await this.apiClient.authFetch(url, {
+            method: "DELETE",
+        });      
+
+        return apiResponse.ok
+    }
 }
 
 export default APICommentService
